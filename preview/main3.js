1c71a86704df29997ce8431660c263f709309e3f
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-30 18:20:36 +0100

    add collapsible

---
+++ preview/main3.js
@@ -0,0 +1,13 @@
+import { Collapsible } from "zextra/surface/collapsible";
+import 'zextra/style'
+import { tags } from "ziko/ui";
+
+globalThis.c = Collapsible(
+    'Summary', 
+    tags.pre({},`
+        ddkd
+        jdjd
+        djdjd
+        jdjdo
+        `)
+)
