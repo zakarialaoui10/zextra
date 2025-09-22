8d142456fd71ca3266b52230d5ad810a6fe22d61
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-09-22 19:23:59 +0100

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-export * from "./accordion";
-export * from "./carousel";
-// export * from "./code-note";
-export * from "./tabs";
-export * from "./alert";
-export * from "./splitter";
-export * from "./pagination";
-export * from "./menu";
-export * from "./modal"
-export * from "./slider"
+export * from './accordion';
+export * from './button';
+export * from './dialog';
+export * from './feedback';
+export * from './inputs';
+export * from './layouts';
+export * from './nav';
+export * from './q';
+export * from './surface';
+export * from './transition';
+export * from './tree';
 
-export * from './canvas'
-export * from './svg'
+// export * from './alert';
+// export * from './splitter';
+// export * from './pagination';
+// export * from './menu';
+// export * from './modal'
+// export * from './slider'
+
+// export * from './canvas'
+// export * from './svg'
