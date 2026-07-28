7c4a970036bb35fa946975cace0d717be003b133
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-07-28 16:10:12 +0100

    .

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+export * from './actions/main.js';
+export * from './containers/main.js';
+export * from './dialog/main.js'; // DEPRECTAED
+export * from './feedback/main.js';
+export * from './inputs/main.js';
+// export * from './layouts/';
+// export * from './lists/';
+export * from './logics/main.js';
+export * from './nav/main.js';
+export * from './reveal/main.js';
+export * from './transition/main.js';
+export * from './typography/main.js';
+export * from './borders/main.js'
+export * from './overlays/main.js'
